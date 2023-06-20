e77a74ab2289eed402e3da9631e233409cd1d4f0
Bernardo Viero <bernardodesouviero@hotmail.com>
2023-06-20 19:57:35 -0300

    instalando styled components e eslint

--- vendasApp/src/App.tsx
+++ vendasApp/src/App.tsx
@@ -1,11 +1,21 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+const TextNew = styled.Text`
+  font-size: 24px;
+  color: red;
+`;
 
 const App = () => {
-    return (
-        <View>
-            <Text>Teste</Text>
-        </View>
-    );
-}
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Teste1</Text>
+        <TextNew>Teste2</TextNew>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default App;
