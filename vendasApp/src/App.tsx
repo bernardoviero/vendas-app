a86a5b7caec0fd5e48ee29b44abf6c4713f30e90
Bernardo Viero <bernardodesouviero@hotmail.com>
2023-06-21 19:08:44 -0300

    ajustes

--- vendasApp/src/App.tsx
+++ vendasApp/src/App.tsx
@@ -1,18 +1,10 @@
-import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
-import styled from 'styled-components/native';
-
-const TextNew = styled.Text`
-  font-size: 24px;
-  color: red;
-`;
 
 const App = () => {
   return (
     <SafeAreaView>
       <View>
-        <Text>Teste1</Text>
-        <TextNew>Teste2</TextNew>
+        <Text>teste1</Text>
       </View>
     </SafeAreaView>
   );
