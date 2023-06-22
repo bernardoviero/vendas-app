a3211ba64d443cde747654bc0f9cbf4e96950401
Bernardo Viero <bernardodesouviero@hotmail.com>
2023-06-22 20:39:42 -0300

    insert type in text

---
+++ vendasApp/react-native.config.js
@@ -0,0 +1,7 @@
+module.export = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['.src/assets/fonts/'],
+};
--- vendasApp/src/shared/components/text/text.style.ts
+++ vendasApp/src/shared/components/text/text.style.ts
@@ -3,10 +3,12 @@ import styled from 'styled-components/native';
 interface ContainerTextProps {
   color?: string;
   fontSize: string;
+  fontFamily: 'Poppins-Bold' | 'Poppins-Regular' | 'Poppins-Light';
 }
 
 export const ContainerText = styled.Text<ContainerTextProps>`
   ${(props) => (props.color ? `color: ${props.color};` : '')}
 
+  font-family: ${(props) => props.fontFamily};
   font-size: ${(props) => props.fontSize};
 `;
