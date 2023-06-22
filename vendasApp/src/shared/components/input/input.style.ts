0d053e14bdec11f858d5106ebd5519c8cc9f22ec
Bernardo Viero <bernardodesouviero@hotmail.com>
2023-06-22 19:09:00 -0300

    create input shared

---
+++ vendasApp/src/shared/components/input/input.style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const ContainerInput = styled.TextInput`
+  width: 100%;
+  height: 48px;
+  padding: 16px;
+  background-color: white;
+  color: #888;
+  border-radius: 4px;
+  padding: 16px;
+`;
