7a75a77a1cf9c36f43003b52c85791d1a19f9f88
Bernardo Viero <bernardodesouviero@hotmail.com>
2023-06-27 18:25:36 -0300

    semi bold add font

--- vendasApp/src/shared/components/button/Button.tsx
+++ vendasApp/src/shared/components/button/Button.tsx
@@ -29,7 +29,7 @@ const Button = ({ title, type, loading, margin, disabled, onPress, ...props }: B
 
   const renderText = (color: string) => (
     <>
-      <Text type={textTypes.BUTTON_REGULAR} color={color}>
+      <Text type={textTypes.BUTTON_SEMI_BOLD} color={color}>
         {title}
       </Text>
       {loading && <ActivityIndicatorButton color={theme.colors.neutralTheme.white} />}
