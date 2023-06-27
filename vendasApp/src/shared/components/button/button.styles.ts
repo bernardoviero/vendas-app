c44fc388bf8d176117dd23aa87994f62953bded0
Bernardo Viero <bernardodesouviero@hotmail.com>
2023-06-27 18:15:27 -0300

    insert loading and disabled in button

--- vendasApp/src/shared/components/button/button.styles.ts
+++ vendasApp/src/shared/components/button/button.styles.ts
@@ -11,6 +11,7 @@ export const ButtonContainer = styled.TouchableOpacity<ButtonContainerProps>`
   width: 100%;
   height: 48px;
   border-radius: 4px;
+  flex-direction: row;
   background-color: blue;
   justify-content: center;
   align-items: center;
@@ -21,6 +22,7 @@ export const GradientButton = styled(LinearGradient)<ButtonContainerProps>`
   width: 100%;
   height: 100%;
   border-radius: 4px;
+  flex-direction: row;
   background-color: blue;
   justify-content: center;
   align-items: center;
@@ -32,4 +34,12 @@ export const ButtonSecondary = styled(ButtonContainer)<ButtonContainerProps>`
   background-color: transparent;
   border-width: 1px;
   border-color: ${theme.colors.mainTheme.primary};
+`;
+
+export const ButtonDisabled = styled(ButtonContainer)<ButtonContainerProps>`
+  background-color: ${theme.colors.grayTheme.gray100};
+`;
+
+export const ActivityIndicatorButton = styled.ActivityIndicator`
+  margin-left: 10px;
 `;
